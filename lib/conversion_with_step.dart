d7a925e85774deb273ce182a7d3ff8d9d9b6b5da
f0ursecond <2ez4second@gmail.com>
2025-03-20 22:06:53 +0700

    feat: finsh

--- lib/conversion_with_step.dart
+++ lib/conversion_with_step.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'step.dart';
+
 void main() {
   bool isContinue = false;
 
@@ -53,9 +55,9 @@ void main() {
             }
             break;
           case "3":
-            try{
+            try {
               print("Biner ke Desimal : ${binerToDecimal(inputNumber ?? "0")}");
-            } catch (e){
+            } catch (e) {
               print("Konversi gagal $e");
             }
             break;
@@ -149,29 +151,28 @@ void main() {
         switch (whatUwantToConvert) {
           case "1":
             try {
-              // TODO : Desimal To Biner
+              print('Desimal ke biner ${decimalToBiner(int.parse(inputNumber ?? "0"))}');
             } catch (e) {
               print("Konversi gagal $e");
             }
-
             break;
           case "2":
             try {
-              // TODO : Desimal To Octal
+              print('Desimal ke oktal ${decimalToOctal(int.parse(inputNumber ?? "0"))}');
             } catch (e) {
               print("Konversi gagal $e");
             }
             break;
           case "3":
             try {
-              // TODO : Desimal To Decimal
+              print(inputNumber);
             } catch (e) {
               print("Konversi gagal $e");
             }
             break;
           case "4":
             try {
-              // TODO : Desimal To Hexa
+              print('Decimal ke hexadesimal ${decimalToHexadecimal(int.parse(inputNumber ?? "0"))}');
             } catch (e) {
               print("Konversi gagal $e");
             }
@@ -205,28 +206,28 @@ void main() {
         switch (whatUwantToConvert) {
           case "1":
             try {
-              // TODO : Hexa To Biner
+              print('Hexa ke Biner ${hexaToBiner(inputNumber ?? "")}');
             } catch (e) {
               print("Konversi gagal $e");
             }
             break;
           case "2":
             try {
-              // TODO : Hexa To Octal
+              print('Hexa ke Oktal ${hexaToOctal(inputNumber ?? "")}');
             } catch (e) {
               print("Konversi gagal $e");
             }
             break;
           case "3":
             try {
-              // TODO : Hexa To Decimal
+              print('Hexa ke Desimal ${hexaToDecimal(inputNumber ?? "")}');
             } catch (e) {
               print("Konversi gagal $e");
             }
             break;
           case "4":
             try {
-              // TODO : Hexa To Hexa
+              print(inputNumber);
             } catch (e) {
               print("Konversi gagal $e");
             }
@@ -249,136 +250,3 @@ void main() {
     }
   } while (isContinue);
 }
-
-
-String binerToOctal(String biner){
-  while (biner.length % 3 != 0) {
-    biner = "0$biner";
-  }
-
-  List<String> binerList = [];
-
-  for (int i = 0; i < biner.length; i += 3){
-    print ("Iterasi ke ${binerList.length + 1} : ${biner.substring(i,i+3)}" );
-    binerList.add(biner.substring(i,i+3));
-  }
-
-  Map<String, String> binerToOctalMap = {
-    "000": "0", "001": "1", "010": "2", "011": "3",
-    "100": "4", "101": "5", "110": "6", "111": "7"
-  };
-
-  print("--------------------+");
-
-  String octal = binerList.map((e) => binerToOctalMap[e]!).join();
-  return octal;
-}
-
-int binerToDecimal(String biner){
-  int pangkat = 0;
-  int decimal = 0;
-
-  for (int i = biner.length - 1; i >= 0 ; i--){
-    int bit = int.parse(biner[i]);
-    int nilaiPangkat = 1 << pangkat;
-    print("$bit x 2^$pangkat = ${bit * nilaiPangkat}");
-    decimal += bit * nilaiPangkat;
-    pangkat++;
-  }
-  print("--------------------+");
-  return decimal;
-}
-
-String binerToHexa(String biner) {
-
-  while (biner.length % 4 != 0) {
-    biner = "0$biner";
-  }
-
-  Map<String, String> hexMap = {
-    "0000": "0", "0001": "1", "0010": "2", "0011": "3",
-    "0100": "4", "0101": "5", "0110": "6", "0111": "7",
-    "1000": "8", "1001": "9", "1010": "A", "1011": "B",
-    "1100": "C", "1101": "D", "1110": "E", "1111": "F"
-  };
-
-  String hexa = "";
-
-  for (int i = 0; i < biner.length; i += 4) {
-    String chunk = biner.substring(i, i + 4);
-    print("Iterasi ke ${hexa.length + 1} : $chunk");
-    hexa += hexMap[chunk]!;
-  }
-
-  print("----------------------+");
-
-  return hexa;
-}
-
-//TODO : Octal To Biner
-
-String octalToBiner(String octal){
-  Map<String, String> octalToBinerMap = {
-    "0": "000", "1": "001", "2": "010", "3": "011",
-    "4": "100", "5": "101", "6": "110", "7": "111"
-  };
-
-  String biner = "";
-
-  for (int i = 0; i < octal.length; i++) {
-    String chunk = octal[i];
-    print("Iterasi ke ${biner.length + 1} : $chunk");
-    biner += octalToBinerMap[chunk]!;
-  }
-
-  print("----------------------+");
-
-  return biner;
-}
-
-//TODO : Octal To Decimal
-
-String octalToDecimal(String octal){
-  int decimal = 0;
-  int pangkat = 0;
-
-  for (int i = octal.length - 1; i >= 0; i--) {
-    int bit = int.parse(octal[i]);
-    int nilaiPangkat = 1 << (3 * pangkat);
-    print("$bit x 8^$pangkat = ${bit * nilaiPangkat}");
-    decimal += bit * nilaiPangkat;
-    pangkat++;
-  }
-  print("--------------------+");
-  return decimal.toString();
-}
-
-//TODO : Octal To Hexa
-
-String octalToHexadecimal(String octal){
-  String decimal = octalToDecimal(octal);
-  return decimalToHexadecimal(int.parse(decimal));
-}
-
-//TODO : Decimal To Biner
-//TODO : Decimal To Octal
-//TODO : Decimal To Hexa
-String decimalToHexadecimal(int decimal) {
-  if (decimal == 0) return "0";
-
-  String hexa = "";
-  String hexDigits = "0123456789ABCDEF";
-
-  while (decimal > 0) {
-    int remainder = decimal % 16;
-    hexa = hexDigits[remainder] + hexa;
-    decimal ~/= 16;
-  }
-
-  return hexa;
-}
-
-
-//TODO : Hexa To Biner
-//TODO : Hexa To Octal
-//TODO : Hexa To Decimal
---
+++ lib/step.dart
@@ -0,0 +1,358 @@
+import 'dart:math';
+
+String binerToOctal(String biner) {
+  while (biner.length % 3 != 0) {
+    biner = "0$biner";
+  }
+
+  List<String> binerList = [];
+
+  for (int i = 0; i < biner.length; i += 3) {
+    print("Iterasi ke ${binerList.length + 1} : ${biner.substring(i, i + 3)}");
+    binerList.add(biner.substring(i, i + 3));
+  }
+
+  Map<String, String> binerToOctalMap = {
+    "000": "0",
+    "001": "1",
+    "010": "2",
+    "011": "3",
+    "100": "4",
+    "101": "5",
+    "110": "6",
+    "111": "7"
+  };
+
+  print("--------------------+");
+
+  String octal = binerList.map((e) => binerToOctalMap[e]!).join();
+  return octal;
+}
+
+int binerToDecimal(String biner) {
+  int pangkat = 0;
+  int decimal = 0;
+
+  print("Langkah langkah konversi biner ke decimal : ");
+
+  for (int i = biner.length - 1; i >= 0; i--) {
+    int bit = int.parse(biner[i]);
+    int nilaiPangkat = 1 << pangkat;
+    print("$bit x 2^$pangkat = ${bit * nilaiPangkat}");
+    decimal += bit * nilaiPangkat;
+    pangkat++;
+  }
+  print("--------------------+");
+  return decimal;
+}
+
+String binerToHexa(String biner) {
+  while (biner.length % 4 != 0) {
+    biner = "0$biner";
+  }
+
+  Map<String, String> hexMap = {
+    "0000": "0",
+    "0001": "1",
+    "0010": "2",
+    "0011": "3",
+    "0100": "4",
+    "0101": "5",
+    "0110": "6",
+    "0111": "7",
+    "1000": "8",
+    "1001": "9",
+    "1010": "A",
+    "1011": "B",
+    "1100": "C",
+    "1101": "D",
+    "1110": "E",
+    "1111": "F"
+  };
+
+  String hexa = "";
+
+  print("Langkah langkah konversi biner ke hexadesimal : ");
+
+  for (int i = 0; i < biner.length; i += 4) {
+    String chunk = biner.substring(i, i + 4);
+    String hexValue = hexMap[chunk]!;
+
+    print("Iterasi ke-${hexa.length + 1}: $chunk → $hexValue");
+    hexa += hexMap[chunk]!;
+  }
+
+  print("----------------------+");
+
+  return hexa;
+}
+
+String octalToBiner(String octal) {
+  Map<String, String> octalToBinerMap = {
+    "0": "000",
+    "1": "001",
+    "2": "010",
+    "3": "011",
+    "4": "100",
+    "5": "101",
+    "6": "110",
+    "7": "111"
+  };
+
+  String biner = "";
+
+  print("Langkah-langkah konversi oktal ke biner:");
+  for (int i = 0; i < octal.length; i++) {
+    String chunk = octal[i];
+    String binerChunk = octalToBinerMap[chunk]!;
+
+    print("Iterasi ke-${i + 1}: $chunk → $binerChunk");
+
+    biner += binerChunk;
+  }
+
+  print("----------------------+");
+
+  return biner;
+}
+
+String octalToDecimal(String octal) {
+  int decimal = 0;
+  int pangkat = 0;
+
+  for (int i = octal.length - 1; i >= 0; i--) {
+    int bit = int.parse(octal[i]);
+    int nilaiPangkat = pow(8, pangkat).toInt();
+    print("$bit x 8^$pangkat = ${bit * nilaiPangkat}");
+    decimal += bit * nilaiPangkat;
+    pangkat++;
+  }
+  print("--------------------+");
+  return decimal.toString();
+}
+
+String octalToHexadecimal(String octal) {
+  int decimal = 0;
+  int pangkat = 0;
+
+  for (int i = octal.length - 1; i >= 0; i--) {
+    int digit = int.parse(octal[i]);
+    int nilaiPangkat = 1 << (3 * pangkat);
+    print("$digit x 8^$pangkat = ${digit * nilaiPangkat}");
+    decimal += digit * nilaiPangkat;
+    pangkat++;
+  }
+
+  print("--------------------+");
+
+  String hexa = "";
+  Map<int, String> hexMap = {10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F"};
+
+  while (decimal > 0) {
+    int sisa = decimal % 16;
+    String hexDigit = sisa < 10 ? sisa.toString() : hexMap[sisa]!;
+    print("Iterasi ke ${hexa.length + 1} : $decimal / 16 = ${decimal ~/ 16}, sisa = $hexDigit");
+    hexa = hexDigit + hexa;
+    decimal ~/= 16;
+  }
+
+  return hexa.isEmpty ? "0" : hexa;
+}
+
+String decimalToBiner(int decimal) {
+  if (decimal == 0) {
+    return "0";
+  }
+
+  String result = "";
+  int num = decimal;
+
+  print("Langkah-langkah konversi Decimal ke Biner:");
+
+  while (num > 0) {
+    int remainder = num % 2;
+    print("$num ÷ 2 = ${num ~/ 2}, sisa = $remainder");
+
+    result = remainder.toString() + result;
+    num = num ~/ 2;
+  }
+
+  print("--------------------+");
+  return result;
+}
+
+String decimalToOctal(int decimal) {
+  if (decimal == 0) {
+    return "0";
+  }
+
+  String result = "";
+  int num = decimal;
+
+  print("Langkah-langkah konversi Decimal ke Oktal:");
+
+  while (num > 0) {
+    int remainder = num % 8;
+    print("$num ÷ 8 = ${num ~/ 8}, sisa = $remainder");
+
+    result = remainder.toString() + result;
+    num = num ~/ 8;
+  }
+
+  print("--------------------+");
+  return result;
+}
+
+String decimalToHexadecimal(int decimal) {
+  if (decimal == 0) return "0";
+
+  String hexa = "";
+  String hexDigits = "0123456789ABCDEF";
+
+  print("Langkah-langkah konversi Decimal ke Hexadesimal:");
+
+  while (decimal > 0) {
+    int remainder = decimal % 16;
+    print("$decimal ÷ 16 = ${decimal ~/ 16}, sisa = ${hexDigits[remainder]}");
+
+    hexa = hexDigits[remainder] + hexa;
+    decimal ~/= 16;
+  }
+
+  print("--------------------+");
+  return hexa;
+}
+
+String hexaToBiner(String hexa) {
+  final Map<String, String> hexToBinary = {
+    '0': '0000',
+    '1': '0001',
+    '2': '0010',
+    '3': '0011',
+    '4': '0100',
+    '5': '0101',
+    '6': '0110',
+    '7': '0111',
+    '8': '1000',
+    '9': '1001',
+    'A': '1010',
+    'B': '1011',
+    'C': '1100',
+    'D': '1101',
+    'E': '1110',
+    'F': '1111',
+    'a': '1010',
+    'b': '1011',
+    'c': '1100',
+    'd': '1101',
+    'e': '1110',
+    'f': '1111'
+  };
+
+  String result = '';
+
+  print("Langkah-langkah konversi Hexa ke Biner:");
+
+  for (int i = 0; i < hexa.length; i++) {
+    String digit = hexa[i];
+    if (hexToBinary.containsKey(digit)) {
+      print("$digit -> ${hexToBinary[digit]}");
+      result += hexToBinary[digit]!;
+    } else {
+      return 'Invalid hexadecimal input';
+    }
+  }
+
+  while (result.startsWith('0') && result.length > 1) {
+    result = result.substring(1);
+  }
+
+  print("--------------------+");
+  return result;
+}
+
+String hexaToOctal(String hexa) {
+  String binary = hexaToBiner(hexa);
+  if (binary == 'Invalid hexadecimal input') {
+    return binary;
+  }
+
+  print(binary);
+
+  print("Biner hasil konversi dari Hexa ($hexa) : $binary");
+
+  // Pastikan panjang biner kelipatan 3
+  while (binary.length % 3 != 0) {
+    binary = '0' + binary;
+  }
+
+  print("Biner setelah penyesuaian ke kelipatan 3 : $binary");
+
+  String result = '';
+  for (int i = 0; i < binary.length; i += 3) {
+    String group = binary.substring(i, i + 3);
+    int value = 0;
+
+    print("Kelompok biner ke-${(i ~/ 3) + 1} : $group");
+
+    for (int j = 0; j < group.length; j++) {
+      value = value * 2 + int.parse(group[j]);
+    }
+
+    print("Nilai oktal dari $group : $value");
+    result += value.toString();
+  }
+
+  print("--------------------+");
+  return result;
+}
+
+String hexaToDecimal(String hexa) {
+  final Map<String, int> hexToDecimal = {
+    '0': 0,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    'A': 10,
+    'B': 11,
+    'C': 12,
+    'D': 13,
+    'E': 14,
+    'F': 15,
+    'a': 10,
+    'b': 11,
+    'c': 12,
+    'd': 13,
+    'e': 14,
+    'f': 15
+  };
+
+  int result = 0;
+  print("Konversi Hexadecimal ke Decimal ($hexa):");
+
+  for (int i = 0; i < hexa.length; i++) {
+    String digit = hexa[i];
+
+    if (!hexToDecimal.containsKey(digit)) {
+      return 'Invalid hexadecimal input';
+    }
+
+    int nilaiDigit = hexToDecimal[digit]!;
+    print("Digit: $digit -> Nilai: $nilaiDigit");
+
+    // Simpan nilai sebelum di-update buat print yang lebih jelas
+    int sebelum = result;
+    result = sebelum * 16 + nilaiDigit;
+
+    print("Total: ($sebelum * 16) + $nilaiDigit = $result");
+  }
+
+  print("--------------------+");
+  return result.toString();
+}
